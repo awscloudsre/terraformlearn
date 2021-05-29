66ea4828881de0e7c38991e2b1599cd91ebfd98f
EC2 Default User <ec2-user@ip-172-31-84-8.ec2.internal>
2021-05-29 18:16:47 +0000

    updated

--- firstterra.tf
+++ firstterra.tf
@@ -38,3 +38,36 @@ resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.main.id
   route_table_id = aws_route_table.example.id
 }
+
+resource "aws_security_group" "main" {
+  name        = "allow_http"
+  description = "Allow http inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "http"
+    cidr_blocks      = ["0.0.0.0/0"]
+}
+}
+resource "aws_instance" "web" {
+  ami           = "ami-0d5eff06f840b45e9"
+  instance_type = "t3.micro"
+  subnet_id = aws_subnet.main.id
+  vpc_security_group_ids = [aws_security_group.main.id]
+  user_data = <<EOF
+                #! /bin/bash
+                sudo yum install java-1.8.0 -y
+                sudo yum update -y
+                sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+                sudo rpm - import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key
+                sudo yum install jenkins -y
+                sudo systemctl start jenkins
+                sudo systemctl enable jenkins
+  EOF
+  tags = {
+    Name = "HelloWorld"
+  }
+}
